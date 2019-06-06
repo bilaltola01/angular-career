9a04ca1b773a18f73ed8921f0f74517faeeb2c30
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-06-06 01:39:49 -0700

    Update style of Login page

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -8,9 +8,33 @@ import { ActionButtonComponent } from './../../components/action-button/action-b
 })
 export class LoginComponent implements OnInit {
 
+  userRoles: object = {
+    'applicant': 0,
+    'recruiter': 1
+  };
+
+  currentRole: number;
+  emailAddress: string;
+  password: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.currentRole = 0;
+    this.emailAddress = '';
+    this.password = '';
   }
+
+  switchRole(role: string) {
+    this.currentRole = this.userRoles[role];
+  }
+
+  checkUserRole(role: string) {
+    return this.currentRole === this.userRoles[role];
+  }
+
+  navigateToSignUp() {}
+
+  login() {}
 
 }
