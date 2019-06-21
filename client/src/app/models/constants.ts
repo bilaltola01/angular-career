043fb8c324023d273f976102f734b0d749d1d892
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-06-21 09:11:46 -0700

    Implement registration flow - Add constants models

---
+++ client/src/app/models/constants.ts
@@ -0,0 +1,14 @@
+export const EthnicityTypes = [
+  'American Indian or Alaska Native',
+  'Asian',
+  'Black or African American',
+  'Hispanic or Latino',
+  'Native Hawaiian or Other Pacific Islander',
+  'White',
+  'Undisclosed'
+];
+
+export const Genders = [
+  'Male',
+  'Female'
+];
