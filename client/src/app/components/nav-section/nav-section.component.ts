de70e9fd36573f05d0a1e38e7e776d92455a015b
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-07-03 19:23:16 -0700

    Nav Section

--- client/src/app/components/nav-section/nav-section.component.ts
+++ client/src/app/components/nav-section/nav-section.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'nav-section',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-section.component.scss']
 })
 export class NavSectionComponent implements OnInit {
+
+  @Input() navMenu: any[];
 
   constructor() { }
 
