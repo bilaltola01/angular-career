a87c3ab41b60256f1d5534d06e3715a69851c881
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-07-25 22:16:37 +0530

    Updated test cases for sevice and component.

--- client/src/app/pages/positions/position-search/position-search.component.spec.ts
+++ client/src/app/pages/positions/position-search/position-search.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PositionSearchComponent } from './position-search.component';
+import { TestingModule } from 'src/app/modules/testing.module';
+import { MaterialModule } from 'src/app/modules/material.module';
 
+import { SimpleTagComponent } from '../../../components/simple-tag/simple-tag.component';
+import { ActionButtonComponent } from '../../../components/action-button/action-button.component';
+import { RoundCheckboxComponent } from '../../../components/round-checkbox/round-checkbox.component';
 describe('PositionSearchComponent', () => {
   let component: PositionSearchComponent;
   let fixture: ComponentFixture<PositionSearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PositionSearchComponent ]
+      imports : [ MaterialModule, TestingModule ],
+      declarations: [ PositionSearchComponent, SimpleTagComponent, ActionButtonComponent, RoundCheckboxComponent ]
     })
     .compileComponents();
   }));
