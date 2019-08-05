5871e894000e052d8d4924cac595bc9b935bbcbe
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-08-04 18:42:57 -0700

    Add Service to manage User information

--- client/src/app/services/index.ts
+++ client/src/app/services/index.ts
@@ -3,3 +3,4 @@ export * from './alerts.service';
 export * from './auto-complete.service';
 export * from './user.service';
 export * from './photo-state.service';
+export * from './user-state.service';
