a96d368a174f720d88d8f83b451040ddec5fe0e0
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-08-17 17:12:56 +0300

    Fixing error where app displays 404 when user credentials are wrong
    Commenting out status code - case 0

--- client/src/app/services/error-interceptor.ts
+++ client/src/app/services/error-interceptor.ts
@@ -4,6 +4,7 @@ import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { UserService } from './user.service';
 import { Router } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -17,24 +18,26 @@ export class ErrorInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request)
       .pipe(catchError(err => {
-        this.determineActionBasedOnStatusCode(Number(err.status));
+        this.determineActionBasedOnStatusCode(Number(err.status), err.url);
 
         return throwError(err);
       }));
   }
 
-  private determineActionBasedOnStatusCode(statusCode: number): void {
+  private determineActionBasedOnStatusCode(statusCode: number, reqUrl: string): void {
     switch (statusCode) {
       case 404: {
-        this.router.navigate(['/error'], {queryParams: {'status-code': statusCode}});
-        break;
+        if (!reqUrl.includes(environment.auth_service)) {
+          this.router.navigate(['/error'], {queryParams: {'status-code': statusCode}});
+          break;
+        }
       }
       // if status code is 0 that means we have server side problem ( wrong headers, token or server is down )
-      case 0: {
-        this.userService.logOut();
-        this.router.navigate(['/login']);
-        break;
-      }
+      // case 0: {
+      //   this.userService.logOut();
+      //   this.router.navigate(['/login']);
+      //   break;
+      // }
     }
   }
 }
