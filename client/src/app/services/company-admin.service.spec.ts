1e8de039062e26a8bd759b6945536403cd1853d0
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-09-04 10:30:04 -0700

    Add testing module for company recruiter service and admins service

--- client/src/app/services/company-admin.service.spec.ts
+++ client/src/app/services/company-admin.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CompanyAdminService } from './company-admin.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CompanyAdminService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [CompanyAdminService]
+  }));
 
   it('should be created', () => {
     const service: CompanyAdminService = TestBed.get(CompanyAdminService);
--- client/src/app/services/company-recruiter.service.spec.ts
+++ client/src/app/services/company-recruiter.service.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CompanyRecruiterService } from './company-recruiter.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CompanyRecruiterService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [CompanyRecruiterService]
+  }));
 
   it('should be created', () => {
     const service: CompanyRecruiterService = TestBed.get(CompanyRecruiterService);
