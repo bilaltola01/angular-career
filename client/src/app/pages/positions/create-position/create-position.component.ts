c274d4e79a66046c6e37457110aa50737c5cd2d1
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-10-07 01:03:20 -0700

    Call post preferred_work_experience API after previous one completed

--- client/src/app/pages/positions/create-position/create-position.component.ts
+++ client/src/app/pages/positions/create-position/create-position.component.ts
@@ -1329,48 +1329,51 @@ export class CreatePositionComponent implements OnInit {
       };
       this.positionService.postLocation(info).subscribe(
         dataJson => {
-          this.addPreferredWorkExperience(position_id);
+          this.addPreferredWorkExperiences(position_id);
         },
         error => {
           this.alertsService.show(error.message, AlertType.error);
-          this.addPreferredWorkExperience(position_id);
+          this.addPreferredWorkExperiences(position_id);
         }
       );
     } else {
-      this.addPreferredWorkExperience(position_id);
+      this.addPreferredWorkExperiences(position_id);
     }
   }
 
-  addPreferredWorkExperience(position_id: number) {
+  addPreferredWorkExperiences(position_id: number) {
     if (this.preferred_work_experiences && this.preferred_work_experiences.length > 0 && this.preferred_work_experiences[0].industry && this.preferred_work_experiences[0].years) {
-      let count = 0;
-      this.preferred_work_experiences.forEach(experience => {
-        const info = {
-          position_id: position_id,
-          industry_id: experience.industry.industry_id,
-          preferred_years: experience.years,
-          exp_desc:	experience.description ? experience.description : null,
-          skills_trained_ids: experience.skills_trained && experience.skills_trained.length > 0 ? experience.skills_trained.map(value => value.skill_id) : null
-        };
-        this.positionService.postPreferredExperience(info).subscribe(
-          dataJson => {
-            count++;
-            if (count === this.preferred_work_experiences.length) {
-              this.openDialog('tamplate', this.position.position);
-            }
-          },
-          error => {
-            this.alertsService.show(error.message, AlertType.error);
-            count++;
-            if (count === this.preferred_work_experiences.length) {
-              this.openDialog('tamplate', this.position.position);
-            }
-          }
-        );
-      });
+      this.addPreferredWorkExperience(position_id, 0);
     } else {
-      this.openDialog('tamplate', this.position.position);
+      this.openDialog('template', this.position.position);
     }
+  }
+
+  addPreferredWorkExperience(position_id: number, arrIndex: number) {
+    const info = {
+      position_id: position_id,
+      industry_id: this.preferred_work_experiences[arrIndex].industry.industry_id,
+      preferred_years: this.preferred_work_experiences[arrIndex].years,
+      exp_desc:	this.preferred_work_experiences[arrIndex].description ? this.preferred_work_experiences[arrIndex].description : null,
+      skills_trained_ids: this.preferred_work_experiences[arrIndex].skills_trained && this.preferred_work_experiences[arrIndex].skills_trained.length > 0 ? this.preferred_work_experiences[arrIndex].skills_trained.map(value => value.skill_id) : null
+    };
+    this.positionService.postPreferredExperience(info).subscribe(
+      dataJson => {
+        if (arrIndex < this.preferred_work_experiences.length - 1) {
+          this.addPreferredWorkExperience(position_id, arrIndex + 1);
+        } else {
+          this.openDialog('template', this.position.position);
+        }
+      },
+      error => {
+        this.alertsService.show(error.message, AlertType.error);
+        if (arrIndex < this.preferred_work_experiences.length - 1) {
+          this.addPreferredWorkExperience(position_id, arrIndex + 1);
+        } else {
+          this.openDialog('template', this.position.position);
+        }
+      }
+    );
   }
 
   saveAsPositionTemplate(position_template_name: string) {
