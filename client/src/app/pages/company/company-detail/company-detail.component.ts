22a3cdcdbf57b811d72ebe2fdc3a9815b77e45f0
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2019-11-26 10:53:08 -0800

    Update routing for company page

--- client/src/app/pages/company/company-detail/company-detail.component.ts
+++ client/src/app/pages/company/company-detail/company-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AlertsService,
+  AlertType,
+  CompanyService
+} from 'src/app/services';
+import { Router, Params, ActivatedRoute } from '@angular/router';
+import { CompanyInfoResponse } from 'src/app/models';
 
 @Component({
   selector: 'app-company-detail',
@@ -7,9 +14,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CompanyDetailComponent implements OnInit {
 
-  constructor() { }
+  company_id: number;
+  showBackButton: boolean;
+  loading: boolean;
+  companyInfo: CompanyInfoResponse;
+
+  constructor(
+    private route: ActivatedRoute,
+    private alertsService: AlertsService,
+    private companyService: CompanyService
+  ) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.loadBasicCompanyInfo();
+  }
+
+  /**
+   * Parse query parameters, Retrieve basic company infomation
+   */
+  loadBasicCompanyInfo() {
+    this.company_id = parseInt(this.route.snapshot.queryParamMap.get('id'), 10) || null;
+    this.showBackButton = this.route.snapshot.queryParamMap.get('showBackButton') === 'true' || false;
+
+    if (this.company_id) {
+      this.getCompanyById(this.company_id);
+    }
+  }
+
+  getCompanyById(company_id: number) {
+    this.companyService.getCompanyById(company_id).subscribe(
+      dataJson => {
+        this.loading = false;
+        this.companyInfo = dataJson['data'];
+      },
+      error => {
+        this.alertsService.show(error.message, AlertType.error);
+      }
+    );
+  }
+
+  /**
+   * Retrieve positions info
+   */
+  loadCompanyPositionsInfo() {
+
+  }
+
+  /**
+   * Retrieve peoples info
+   */
+  loadCompanyPeoplesInfo() {
+
+  }
+
+  /**
+   * Retrieve CareerFair Info
+   */
+  loadCareerFairInfo() {
+
   }
 
 }
--- client/src/app/pages/company/company.routing.ts
+++ client/src/app/pages/company/company.routing.ts
@@ -2,8 +2,14 @@ import { AuthGuard } from '../../guard/auth.guard';
 import { Routes } from '@angular/router';
 import { CreateCompanyComponent } from './create-company/create-company.component';
 import { CompaniesComponent } from './companies/companies.component';
+import { CompanyDetailComponent } from './company-detail/company-detail.component';
 
 export const companyRoutes: Routes = [
+  {
+    path: '',
+    redirectTo: 'companies',
+    pathMatch: 'full'
+  },
   {
     path: 'create-company',
     component: CreateCompanyComponent,
@@ -13,6 +19,10 @@ export const companyRoutes: Routes = [
     path: 'companies',
     component: CompaniesComponent,
     canActivate: [AuthGuard]
+  },
+  {
+    path: 'company-info',
+    component: CompanyDetailComponent,
+    canActivate: [AuthGuard]
   }
-
 ];
