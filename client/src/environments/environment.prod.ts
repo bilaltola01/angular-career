154e03ce9673a861dc2320f569a569a7d281134e
bilaltola01 <25662916+bilaltola01@users.noreply.github.com>
2020-01-28 01:52:56 -0500

    CAR-000 Issues with environment prod file.

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -10,18 +10,21 @@ export const environment = {
   position_service: 'position-service',
   position_management_service: 'position-management-service',
   cart_service: 'cart-service',
+  careerfair_service: 'careerfair-service',
+  matching_service: 'matching-service',
   company_service: 'companies-service',
+  score_service: 'score-service',
   recruiter_service: 'recruiter-service',
   company_admin_service: 'company-admin-service',
   api_version: 'v1',
   firebase: {
-        apiKey: 'AIzaSyDixV2JhwZ6ztWJZvyibry_nvhy0lyXmlA',
-        authDomain: 'cf-chat-8efa4.firebaseapp.com',
-        databaseURL: 'https://cf-chat-8efa4.firebaseio.com',
-        projectId: 'cf-chat-8efa4',
-        storageBucket: 'cf-chat-8efa4.appspot.com',
-        messagingSenderId: '1008369027673',
-        appId: '1:1008369027673:web:88618bb4db6787f6053f48',
-        measurementId: 'G-MQ5MTB2RJV'
+    apiKey: 'AIzaSyDixV2JhwZ6ztWJZvyibry_nvhy0lyXmlA',
+    authDomain: 'cf-chat-8efa4.firebaseapp.com',
+    databaseURL: 'https://cf-chat-8efa4.firebaseio.com',
+    projectId: 'cf-chat-8efa4',
+    storageBucket: 'cf-chat-8efa4.appspot.com',
+    messagingSenderId: '1008369027673',
+    appId: '1:1008369027673:web:88618bb4db6787f6053f48',
+    measurementId: 'G-MQ5MTB2RJV'
   }
 };
